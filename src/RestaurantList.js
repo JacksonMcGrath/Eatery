4b17405c119e493f95c3ea4d449a7d57beae89d7
IamCharlesM <because@charlesis.me>
2018-02-15 14:47:09 -0600

    Restaurant list updated.

--- src/RestaurantList.js
+++ src/RestaurantList.js
@@ -5,9 +5,13 @@ class Restaurants extends Component {
         // console.log("restaurants are ready")
         const allRestaurants = this.props.restaurants.map((restaurants, i) => {
             return <li key={i} >{restaurants.name} - {restaurants.location}: {restaurants.description} </li>})
+
         return (
             <div>
+                 <h1>This is the Restaurants list</h1>
+                <ul>
                 {allRestaurants}
+                </ul>
             </div>
         );
     }
