5af63bf95a9436f80559284f9bd6454d28a54e6f
IamCharlesM <because@charlesis.me>
2018-02-15 12:47:50 -0600

    Reataurants now render on the page.

--- src/RestaurantList.js
+++ src/RestaurantList.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 
 class Restaurants extends Component {
     render() {
-        console.log("restaurants are ready")
+        // console.log("restaurants are ready")
+        const allRestaurants = this.props.restaurants.map((restaurants, i) => {
+            return <li key={i} >{restaurants.name} - {restaurants.location}: {restaurants.description} </li>})
         return (
             <div>
-                <h2>The Restaurants page is rendering</h2>
+                {allRestaurants}
             </div>
         );
     }
